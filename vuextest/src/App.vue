35881ec626b444cd69bad081a5d837d50bfb7a94
mozlee <lixin@mozlee.com>
2018-04-11 16:43:34 +0800

    VUEX

---
+++ vuextest/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png"  @click="$store.state.count++">
+    <HelloWorld/>
+    <h2>{{$store.state.count}}</h2>
+  </div>
+</template>
+<script>
+import HelloWorld from './components/add'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
