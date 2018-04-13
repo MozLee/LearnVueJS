d6c844b2e45c4f7be78c44605b253ef10a4fad2b
mozlee <lixin@mozlee.com>
2018-04-13 14:57:09 +0800

    format

--- vuextest/src/store/index.js
+++ vuextest/src/store/index.js
@@ -1,42 +1,49 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import {getList,getUsers} from '@/server';
+import {
+  getList,
+  getUsers
+} from '@/server';
 
 Vue.use(Vuex);
 
 let store = new Vuex.Store({
-    state:{
-        shopList:[],
-        recommendList:[],
-        listLoading:true,
-        recommendLoadig:true,
-        shopListNum:0,
+  state: {
+    shopList: [],
+    recommendList: [],
+    listLoading: true,
+    recommendLoadig: true,
+    shopListNum: 0,
+  },
+  mutations: {
+    changeList(state, newdata) {
+      // console.log(newdata);
+      state.shopList = newdata;
+      state.listLoading = !state.listLoading;
+      // console.log(state.shopList);
     },
-    mutations:{
-        changeList(state,newdata){
-            // console.log(newdata);
-            state.shopList=newdata;
-            state.listLoading=!state.listLoading;  
-            // console.log(state.shopList);
-        },
-        changeRecommendList(state,newdata){
-            state.recommendList = newdata;
-            state.recommendLoadig = !state.recommendLoadig;
-        }
-    },
-    actions:{
-        getListAction(store){
-            getList().then((data) => {
-                store.commit('changeList',{list:data.data.data.list})
-            })
-        },
-        getRecommendedListAction(store){
-            getUsers().then((data) => {
-                console.log(data);
-                store.commit('changeRecommendList',{list:data.data.data.list})
-            })
-        }
+    changeRecommendList(state, newdata) {
+      state.recommendList = newdata;
+      state.recommendLoadig = !state.recommendLoadig;
     }
+  },
+  actions: {
+    getListAction(store) {
+      getList().then((data) => {
+        store.commit('changeList', {
+          list: data.data.data.list
+        })
+      })
+    },
+    getRecommendedListAction(store) {
+      getUsers().then((data) => {
+        console.log(data);
+        store.commit('changeRecommendList', {
+          list: data.data.data.list
+        })
+      })
+    }
+  }
 })
 
 export default store;
